703cdf58469d44447e87fda9dd908ed8ca7666b0
Richard Mlambuzi <bed-com-29-19@unima.ac.mw>
2021-11-29 23:01:52 +0200

    these files are for testing

---
+++ TaskNonThreadDemo.java
@@ -0,0 +1,32 @@
+d lower case
+ characters separately and Unicode.
+Homepage: http://sourceforge.net/projects/crunch-wordlist/
+
+Package: cryptcat
+Status: install ok unpacked
+Priority: optional
+Section: net
+Installed-Size: 81
+Maintainer: Kali Developers <devel@kali.org>
+Architecture: amd64
+Version: 20031202-5kali4
+Depends: libc6 (>= 2.14), libgcc-s1 (>= 3.0), libstdc++6 (>= 5)
+Description: lightweight version netcat extended with twofish encryption
+ Cryptcat is a simple Unix utility which reads and writes data across
+ network connections, using TCP or UDP protocol while encrypting the
+ data being transmitted.
+ It is designed to be a reliable "back-end" tool that can be used directly
+ or easily driven by other programs and scripts.  At the same time, it is a
+ feature-rich network debugging and exploration tool, since it can create
+ almost any kind of connection you would need and has several interesting
+ built-in capabilities.
+Homepage: http://farm9.com/content/Free_Tools/cryptcat_linux2.tar
+Original-Maintainer: Lars Bahner <bahner@debian.org>
+
+Package: cryptsetup
+Status: install ok unpacked
+Priority: optional
+Section: admin
+Installed-Size: 407
+Maintainer: Debian Cryptsetup Team <pkg-cryptsetup-devel@alioth-lists.debian.net>
+Architecture: amd6
---
+++ rich.java
@@ -0,0 +1,8 @@
+public class rich{
+    public static void main(String[] args) {
+        int k =11;
+        int w = 23;
+        float answer = (k^w);
+        System.out.println(answer);
+    }
+}
